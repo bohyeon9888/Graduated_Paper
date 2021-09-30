0c6f14d96bef754406915956d7b470bad2560af5
bohyeon9888 <bohyeon9888@gmail.com>
2021-09-30 14:23:52 +0900

    modify path of filebench

--- sample.sh
+++ sample.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 XSB_PATH=/home/stu1/XSBench/openmp-threading
-FILEB_PATH=
+FILEB_PATH=/home/stu1/filebench-1.5-alpha3/workloads
 
 OUTPUT_ANON=Anon_usage.out
 OUTPUT_ANONHUGE=Anonhuge_usage.out
 
 TIME_SEQ=$(seq 0 300)
 
+#THP enable
+echo always > /sys/kernel/mm/transparent_hugepage/enabled
+echo always > /sys/kernel/mm/transparent_hugepage/defrag
+
 #execute bench
 ${XSB_PATH}/XSBench -t 4 -g 60000 -p 9000000 &
-
-#THP enable
-echo always > /sys/kernel/mm/transparent_hugepage/enabled 
-echo always > /sys/kernel/mm/transparent_hugepage/defrag
 
 #log
 for i in ${TIME_SEQ}
