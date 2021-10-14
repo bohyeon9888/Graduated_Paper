7b523a66e2f5d5792beb0bf4619fbcc7e7d9571c
bohyeon9888 <bohyeon9888@gmail.com>
2021-10-14 11:44:14 +0900

    Update exp5.sh

--- exp5/exp5.sh
+++ exp5/exp5.sh
@@ -3,14 +3,14 @@
 XSB_PATH=/home/stu1/XSBench/openmp-threading
 BTR_PATH=/home/stu1//vmitosis-workloads/bin
 
-OUTPUT_ANON=BTR_usage_always.out
-OUTPUT_ANONHUGE=BTRhuge_usage_always.out
+OUTPUT_ANON=FX_usage_defer.out
+OUTPUT_ANONHUGE=FXhuge_usage_defer.out
 
 TIME_SEQ=$(seq 0 600)
 
 #execute bench
-#${XSB_PATH}/XSBench -t 4 -g 60000 -p 14000000 &
-${BTR_PATH}/bench_btree_st
+${XSB_PATH}/XSBench -t 4 -g 60000 -p 14000000 &
+#${BTR_PATH}/bench_btree_st
 
 #log
 for i in ${TIME_SEQ}
