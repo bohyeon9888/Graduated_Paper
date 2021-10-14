be3406b6c153397d0c4af3c883a50ad88230bebe
bohyeon9888 <bohyeon9888@gmail.com>
2021-10-14 21:58:56 +0900

    Update exp5.sh

--- exp5/exp5.sh
+++ exp5/exp5.sh
@@ -3,22 +3,20 @@
 XSB_PATH=/home/stu1/XSBench/openmp-threading
 BTR_PATH=/home/stu1//vmitosis-workloads/bin
 
-OUTPUT_ANON=FX_usage_defer.out
-OUTPUT_ANONHUGE=FXhuge_usage_defer.out
+OUTPUT_ANON=BTR_usage_always.out
 
-TIME_SEQ=$(seq 0 600)
+TIME_SEQ=$(seq 0 1000)
 
 #execute bench
-${XSB_PATH}/XSBench -t 4 -g 60000 -p 14000000 &
-#${BTR_PATH}/bench_btree_st
+#time ${XSB_PATH}/XSBench -t 4 -g 50000 -p 14000000 &
+time ${BTR_PATH}/bench_btree_st &
 
 #log
 for i in ${TIME_SEQ}
 do
         echo "time: ${i}s" >> ${OUTPUT_ANON}
-        echo "time: ${i}s" >> ${OUTPUT_ANONHUGE}
         cat /proc/meminfo | grep AnonPage >> ${OUTPUT_ANON}
-        cat /proc/meminfo | grep AnonHuge >> ${OUTPUT_ANONHUGE}
+        cat /proc/meminfo | grep AnonHuge >> ${OUTPUT_ANON}
 
         sleep 1
 done
